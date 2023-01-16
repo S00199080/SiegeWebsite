81192113f49fdddfd4788551132aa6c7e62fd0e9
S00199080 <S00199080@atu.ie>
2023-01-16 21:40:19 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,52 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+    <h1>Rainbow Six Siege Operator Randomizer</h1>
+    <button (click)="randomizeDefenders()">Randomize Defenders</button>
+    <div *ngIf="teamType === 'defenders'">
+      <div *ngFor="let defender of team">
+        {{ defender }}
+      </div>
+    </div>
+    <button (click)="randomizeAttackers()">Randomize Attackers</button>
+    <div *ngIf="teamType === 'attackers'">
+      <div *ngFor="let attacker of team">
+        {{ attacker }}
+        </div>
+      </div>
+    </div>
+  `
+  
 })
 export class AppComponent {
+
   title = 'SiegeApp';
+
+  teamType = 'defenders';
+  
+  defenders = [    "Castle", "Pulse", "Rook", "Doc", "Kapkan", "Tachanka", "Smoke", "Mute", "Valkyrie", "Caveira", "Echo", "Mira", "Lesion", "Ela" ];
+  attackers = [    "Test"];
+
+  team: string[] = [];
+
+  randomizeDefenders() {
+    this.teamType = 'defenders'
+    this.team = [];
+    for (let i = 0; i < 5; i++) {
+      const randomIndex = Math.floor(Math.random() * this.defenders.length);
+      this.team.push(this.defenders[randomIndex]);
+    }
+  }
+
+  randomizeAttackers() {
+    this.teamType = 'attackers'
+    this.team = [];
+    for (let i = 0; i < 5; i++) {
+      const randomIndex = Math.floor(Math.random() * this.attackers.length);
+      this.team.push(this.attackers[randomIndex]);
+    }
+  }
 }
+
